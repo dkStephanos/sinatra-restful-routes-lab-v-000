bd2f633803732ddf6c9dc1a624e19a4d1f04aab7
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-26 17:52:58 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,7 +47,7 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     @recipe = Recipe.create(params)
-    redirect '/recipes/#{@recipe.id}'
+    redirect '/recipes/:id'
   end
 
 end
